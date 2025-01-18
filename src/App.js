62fb87a5c6d961edf398cbe3882db17f831f5bd0
Muzi Phage <muzi.phage@gmail.com>
2025-01-18 12:53:18 +0200

    Conditional rendering using ternary operator

--- src/App.js
+++ src/App.js
@@ -7,25 +7,36 @@ function App() {
   const [name, setName] = useState("Muzi Phage");
   const changeNameHandler = (name) => {
     setName(name);
-  }
+  };
+
+  // set up the logic to toggle - render on condtion
+  const [showCard, setShowCard] = useState(true);
+  const toggleShowCard = () => setShowCard(!showCard);
 
   // Handle value from the input element and add as input to the setName(set State)
-  const changeInputHandler = event => setName(event.target.value)
-  const buttonsMarkup = <div>
-        <button className="button button2">YES</button>
-        <button className="button button3">NO</button>
-  </div>
+  const changeInputHandler = (event) => setName(event.target.value);
+  const buttonsMarkup = (
+    <div>
+      <button className="button button2">YES</button>
+      <button className="button button3">NO</button>
+    </div>
+  );
   return (
     <div className="App">
-      <button className="button" onClick={() => changeNameHandler("Muzi Outer Phage")}>Change Name</button>
-      <Card
-        avatar="https://avatars.githubusercontent.com/u/387560"
-        name={name}
-        title="Product Infrastructure Liaison"
-        onChangeName={() => changeNameHandler("Liyanda Inner Mpanza")}
-        onChangeInput={changeInputHandler}
-      >{buttonsMarkup}
-      </Card>
+      <button className="button" onClick={toggleShowCard}>
+        Toggle Show/Hide Card
+      </button>
+      {showCard ? (
+        <Card
+          avatar="https://avatars.githubusercontent.com/u/387560"
+          name={name}
+          title="Product Infrastructure Liaison"
+          onChangeName={() => changeNameHandler("Liyanda Inner Mpanza")}
+          onChangeInput={changeInputHandler}
+        >
+          {buttonsMarkup}
+        </Card>
+      ) : null}
     </div>
   );
 }
