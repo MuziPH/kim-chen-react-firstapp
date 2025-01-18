6ec80b2f26d61d845c42dc0542ae589d3e4b9e40
Muzi Phage <muzi.phage@gmail.com>
2025-01-18 13:03:10 +0200

    Cleaner Condtional Rendering

--- src/App.js
+++ src/App.js
@@ -21,22 +21,22 @@ function App() {
       <button className="button button3">NO</button>
     </div>
   );
+  // Cleaner way of conditional rendering by using a variable in the Javascript area
+  const cardMarkup = showCard && (
+    <Card
+      avatar="https://avatars.githubusercontent.com/u/387560"
+      name={name}
+      title="Product Infrastructure Liaison"
+      onChangeName={() => changeNameHandler("Liyanda Inner Mpanza")}
+      onChangeInput={changeInputHandler}
+    >
+      {buttonsMarkup}
+    </Card>
+  );
   return (
     <div className="App">
-      <button className="button" onClick={toggleShowCard}>
-        Toggle Show/Hide Card
-      </button>
-      {showCard ? (
-        <Card
-          avatar="https://avatars.githubusercontent.com/u/387560"
-          name={name}
-          title="Product Infrastructure Liaison"
-          onChangeName={() => changeNameHandler("Liyanda Inner Mpanza")}
-          onChangeInput={changeInputHandler}
-        >
-          {buttonsMarkup}
-        </Card>
-      ) : null}
+      <button className="button" onClick={toggleShowCard}>Toggle Show/Hide Card</button>
+      {cardMarkup}
     </div>
   );
 }
